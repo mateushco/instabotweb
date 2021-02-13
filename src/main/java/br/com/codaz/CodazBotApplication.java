4da69d3bb44c8364f99d84ffcd76abd8669532eb
Mateus Oliveira <39748847+mateushco@users.noreply.github.com>
2021-02-12 23:24:05 -0300

    Bot codaz email

--- src/main/java/br/com/codaz/CodazBotApplication.java
+++ src/main/java/br/com/codaz/CodazBotApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import br.com.codaz.services.Email;
 import br.com.codaz.services.Execute;
 
 @SpringBootApplication
@@ -22,7 +23,7 @@ public class CodazBotApplication extends SpringBootServletInitializer{
 		Timer timer = new Timer(); 
 		timer.scheduleAtFixedRate(new TimerTask() {
 		    @Override public void run() { 
-		    	
+		    	Email.teste();
 		    	try {
 					Execute.conectarSeguir("neovitalle", "neovitallesaude17", "conhecaunai", 50, 15);
 					Execute.conectarSeguir("dra.solangebanoski", "sol260879", "conhecaunai", 50, 15);
